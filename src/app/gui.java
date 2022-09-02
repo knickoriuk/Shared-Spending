58cbaf3ccd9a1cc33f9cb4ca08dbc8d2502afbd8
knickoriuk <nickoriuk@live.com>
2022-09-02 16:56:05 -0300

    Updated GUI
    - When adding a purchase, there is now an option for Category
    - Name and Category inputs are now drop-down boxes of values already in the database
    - Purchase history shows purchase category now
    - Added a "Simple Summary" toggle to the Edit menubar
    - More help items are available in the menu
    - Date picker bar is now the same size as the other input bars

--- src/app/gui.java
+++ src/app/gui.java
@@ -5,10 +5,13 @@ import javax.swing.filechooser.*;
 import java.awt.*;
 import java.io.File;
 import java.text.*;
+import java.util.ArrayList;
 import java.util.Properties;
 import org.jdatepicker.impl.*;
 import except.*;
 import db_control.*;
+import static app.string_reformatter.to_titlecase;
+import static app.summary.summarize_db;
 
 public class gui {
 
@@ -16,9 +19,12 @@ public class gui {
     public static final Font font_1 = new Font(Font.SANS_SERIF, Font.PLAIN,18); // Bigger, for text fields and summaries
     public static final Font font_2 = new Font(Font.MONOSPACED, Font.BOLD,14); // monospace, for tables.
 
+    // Simple Summary Toggle
+    public static Boolean simple = false;
+
     // Important components
     public static JFrame frame = new JFrame("Shared Spending Manager");
-    public static JTextArea summary = new JTextArea(get.summary());
+    public static JTextArea summary = new JTextArea(summarize_db(simple));
     public static JPanel panel_home = new JPanel();
     public static JPanel panel_summary = new JPanel(new BorderLayout());
     public static JPanel panel_add_new = new JPanel();
@@ -26,6 +32,8 @@ public class gui {
     public static JMenuBar menu = new JMenuBar();
     public static JTable table_latest = new JTable();
     public static JScrollPane scroll_pane_latest = new JScrollPane();
+    public static JComboBox input_name = new JComboBox();
+    public static JComboBox input_category = new JComboBox();
 
     public static void run_gui() {
         initialize_components();
@@ -43,7 +51,7 @@ public class gui {
     private static void initialize_components() {
         // JFrame
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(425, 410);
+        frame.setSize(450, 475);
 
         // JTextArea summary panel
         summary.setEditable(false);
@@ -61,7 +69,11 @@ public class gui {
         JMenuItem reset_db = make_menu_item("Reset Database", 'R');
         JMenuItem add_one = make_menu_item("Add Entry", 'A');
         JMenuItem remove_one = make_menu_item("Remove Entry", 'R');
-        JMenuItem info = make_menu_item("Info", 'I');
+        JMenuItem info = make_menu_item("General Usage", 'G');
+        JMenuItem info_categories = make_menu_item("Categories", 'C');
+        JMenuItem info_export = make_menu_item("Database Backup", 'D');
+        JCheckBoxMenuItem toggle_simple = new JCheckBoxMenuItem("Simple Summaries");
+        toggle_simple.setMnemonic('S');
 
         // Menu Options
         JMenu mb_file = make_menu_option("File", 'F');
@@ -71,9 +83,13 @@ public class gui {
         mb_file.add(export_csv);
         mb_file.addSeparator();
         mb_file.add(reset_db);
+        mb_edit.add(toggle_simple);
+        mb_edit.addSeparator();
         mb_edit.add(add_one);
         mb_edit.add(remove_one);
         mb_help.add(info);
+        mb_help.add(info_categories);
+        mb_help.add(info_export);
 
         // Menu Listeners
         export_csv.addActionListener(e -> export_csv());
@@ -81,7 +97,13 @@ public class gui {
         reset_db.addActionListener(e -> reset_database());
         remove_one.addActionListener(e -> remove_entry());
         add_one.addActionListener(e -> replace_lower_panel(panel_add_new));
-        info.addActionListener(e -> get_help());
+        info.addActionListener(e -> get_general_help());
+        info_categories.addActionListener(e -> get_category_help());
+        info_export.addActionListener(e -> get_export_help());
+        toggle_simple.addActionListener(e -> {
+            simple = !simple;
+            summary.setText(summarize_db(simple));
+        });
 
         // Construct Menu
         menu.add(mb_file);
@@ -98,7 +120,7 @@ public class gui {
         b_home_display.addActionListener(e -> {
             // Rebuild the JTable each time in case database content has changed
             scroll_pane_latest.getViewport().remove(table_latest); // Remove old table
-            table_latest = new JTable(get.most_recent_purchases(), new String[] {"Date", "Name", "Cost"}); // Rebuild table
+            table_latest = new JTable(get.most_recent_purchases(), new String[] {"Date", "Name", "Cost", "Category"}); // Rebuild table
             table_latest.setFont(font_2);
             table_latest.getTableHeader().setFont(font_1);
             table_latest.setDefaultEditor(Object.class, null); // Prevents editing the table
@@ -117,11 +139,14 @@ public class gui {
         JButton b_new_done = make_button("Add", 'A');
         JButton b_new_cancel = make_button("Cancel", 'C');
 
-        // Text fields
+        // Fields for amount and dropdown name/category selection
         JFormattedTextField input_cost = new JFormattedTextField(new DecimalFormat("0.00"));
         input_cost.setPreferredSize(new Dimension(200,20));
-        JTextField input_name = new JTextField();
+        populate_comboboxes();
+        input_name.setEditable(true);
         input_name.setPreferredSize(new Dimension(200,20));
+        input_category.setEditable(true);
+        input_category.setPreferredSize(new Dimension(200,20));
 
         // Date field
         Properties p = new Properties();
@@ -131,27 +156,46 @@ public class gui {
         UtilDateModel model = new UtilDateModel();
         JDatePanelImpl date_panel = new JDatePanelImpl(model, p);
         JDatePickerImpl input_date = new JDatePickerImpl(date_panel, new date_label_formatter());
+        input_date.setPreferredSize(new Dimension(200,20));
+        input_date.getComponent(0).setPreferredSize(new Dimension(200,20)); // TextField Dimensions
+        input_date.getComponent(1).setPreferredSize(new Dimension(17,20)); // Button Dimensions
 
         // Labels
         JLabel l_cost = new JLabel("Cost of Purchase: $");
         JLabel l_name = new JLabel("Name of Buyer:   ");
         JLabel l_date = new JLabel("Date of Purchase:   ");
+        JLabel l_cat = new JLabel("Purchase Category:   ");
 
         // Button Listeners
         b_new_cancel.addActionListener(e -> replace_lower_panel(panel_home));
         b_new_done.addActionListener(e -> {
+            // Get cost amount and date as strings
             String cost = input_cost.getText();
-            String name = input_name.getText();
             String date = input_date.getJFormattedTextField().getText();
-            try {
-                add.purchase(cost, date, name); // Add to database
-                input_cost.setText(null);// Empty fields
-                input_name.setText(null);
-                input_date.getJFormattedTextField().setText(null);
-                replace_lower_panel(panel_home); // Go back to homepage
-                summary.setText(get.summary()); // Reset summary text
 
-                // Show error messages for bad input
+            String category; // Get category from dropbox, unless empty
+            if (input_category.getSelectedItem() == null) {
+                category = "";
+            } else {
+                category = input_category.getSelectedItem().toString();
+            }
+
+            String name; // Get name from dropbox, unless empty
+            if (input_name.getSelectedItem() == null) {
+                name = "";
+            } else {
+                name = input_name.getSelectedItem().toString();
+            }
+
+            try {
+                add.purchase(cost, date, name, category); // Add to database
+                input_cost.setText(null);// Empty fields
+                input_date.getJFormattedTextField().setText(null);
+                populate_comboboxes();
+                replace_lower_panel(panel_home); // Go back to homepage
+                summary.setText(summarize_db(simple)); // Reset summary text
+
+            // Show error messages for bad input
             } catch (Exception ex) {
                 show_error(ex);
             }
@@ -161,20 +205,25 @@ public class gui {
         JPanel flow_cost = new JPanel();
         JPanel flow_name = new JPanel();
         JPanel flow_date = new JPanel();
+        JPanel flow_cat = new JPanel();
         JPanel flow_commands = new JPanel();
         flow_cost.setLayout(new FlowLayout(FlowLayout.RIGHT));
         flow_name.setLayout(new FlowLayout(FlowLayout.RIGHT));
         flow_date.setLayout(new FlowLayout(FlowLayout.RIGHT));
+        flow_cat.setLayout(new FlowLayout(FlowLayout.RIGHT));
         flow_commands.setLayout(new FlowLayout(FlowLayout.CENTER));
         flow_cost.add(l_cost);
         flow_cost.add(input_cost);
-        flow_cost.add(Box.createHorizontalStrut(40));
+        flow_cost.add(Box.createHorizontalStrut(30));
         flow_name.add(l_name);
         flow_name.add(input_name);
-        flow_name.add(Box.createHorizontalStrut(40));
+        flow_name.add(Box.createHorizontalStrut(30));
         flow_date.add(l_date);
         flow_date.add(input_date);
-        flow_date.add(Box.createHorizontalStrut(40));
+        flow_date.add(Box.createHorizontalStrut(30));
+        flow_cat.add(l_cat);
+        flow_cat.add(input_category);
+        flow_cat.add(Box.createHorizontalStrut(30));
         flow_commands.add(b_new_done);
         flow_commands.add(b_new_cancel);
 
@@ -182,6 +231,7 @@ public class gui {
         panel_add_new.setLayout(new BoxLayout(panel_add_new, BoxLayout.Y_AXIS));
         panel_add_new.add(flow_date);
         panel_add_new.add(flow_name);
+        panel_add_new.add(flow_cat);
         panel_add_new.add(flow_cost);
         panel_add_new.add(flow_commands);
 
@@ -329,7 +379,8 @@ public class gui {
 
                     // Clear the date from the database.
                     db.clear_date(date_input);
-                    summary.setText(get.summary());
+                    summary.setText(summarize_db(simple));
+                    populate_comboboxes();
                     date_dialog.dispose();
                     date_dialog.setVisible(false);
 
@@ -351,7 +402,8 @@ public class gui {
         if (choice == JOptionPane.YES_OPTION) {
             db.clear_table(); // Delete database entries and drop table
             db.initialize(); // Set up new, empty database
-            summary.setText(get.summary()); // Refresh summary text
+            summary.setText(summarize_db(simple)); // Refresh summary text
+            populate_comboboxes();
         }
     }
 
@@ -397,7 +449,8 @@ public class gui {
                     db.clear_table(); // db must be cleared since some data (but not all) may have been entered
                     db.initialize();
                 } finally {
-                    summary.setText(get.summary());
+                    summary.setText(summarize_db(simple));
+                    populate_comboboxes();
                     SwingUtilities.updateComponentTreeUI(frame);
                 }
             }
@@ -407,15 +460,64 @@ public class gui {
     /**
      * Creates a popup with some basic usage information.
      */
-    private static void get_help() {
+    private static void get_general_help() {
         JOptionPane.showMessageDialog(frame,
                 "<html><body><p style='width: 250px;'>The <i>Shared Spending Manager</i> tracks how much " +
                         "money has been spent between a number of individuals. It will display how much each person " +
-                        "has spent, who has spent the least, and a weighted average of spending per day.\n\n" +
+                        "has spent, who has spent the least, and a weighted average of spending per day.</p>\n\n" +
                         "<html><body><p style='width: 250px;'>Add a new record to the database with " +
-                        "\"Add New Purchase\". Ensure that names are spelled the same each time. If a mistake has " +
-                        "been made, you can clear the entries for a specified day with Edit>Remove Entry.",
+                        "\"Add New Purchase\". If a mistake has been made, you can clear the entries for a " +
+                        "specified day with <code>Edit->Remove Entry</code>.</p>",
                 "Info",
                 JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    /**
+     * Creates a popup with some information on categories.
+     */
+    private static void get_category_help() {
+        JOptionPane.showMessageDialog(frame,
+                "<html><body><p style='width: 250px;'>Each purchase can optionally be categorized with a " +
+                        "given label. Having multiple categories for purchases will result in separate categorical " +
+                        "summaries; only comparing spending within the same category. A summary will be generated for " +
+                        "each category, as well as separated averages.</p>\n\n<html><body><p style='width: 250px;'>" +
+                        "Regardless of whether or not categories are used, a summary is always generated for " +
+                        "totals across all purchases in the database.</p>",
+                "Categories Help",
+                JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    /**
+     * Creates a popup with some information on exporting and importing the database.
+     */
+    private static void get_export_help() {
+        JOptionPane.showMessageDialog(frame,
+                "<html><body><p style='width: 250px;'>The database can be backed up by exporting it to a " +
+                        ".csv file. Use <code>File->Export to .csv</code> to generate a .csv file from the database." +
+                        "</p>\n\n<html><body><p style='width: 250px;'>A database export can be imported to the " +
+                        "program with <code>File->Import from .csv</code>. This will overwrite the contents of the " +
+                        "database, but restore it to when it was last exported.</p>",
+                "Exporting & Importing Help",
+                JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    /**
+     * Fills Name-Selection/Category Selection ComboBoxes options with all names/categories that
+     * currently exist in the database
+     */
+    private static void populate_comboboxes() {
+        // Populate Names Dropdown Box
+        ArrayList<String> names = to_titlecase(get.names()); // Get all names in the db
+        input_name.removeAllItems(); // Remove preexisting options in the drop-down menu
+        for (String name : names) { input_name.addItem(name); } // Fill with names from list
+        input_name.setSelectedItem(null);
+
+        // Populate Categories Dropdown Box
+        ArrayList<String> categories = to_titlecase(get.categories()); // Get all names in the db
+        input_category.removeAllItems(); // Remove preexisting options in the drop-down menu
+        for (String category : categories) { // Fill with categories from list
+            if (!category.equals("Uncategorized")) { input_category.addItem(category); }
+        }
+        input_category.setSelectedItem(null);
     }
 }
